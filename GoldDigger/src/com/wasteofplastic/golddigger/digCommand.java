11409f16cfc3dfef6a19dde352cf5cab22e2075d
tastybento <tastybento@wasteofplastic.com>
2015-05-05 20:52:34 -0700

    Mavenized. Changed API to Bukkit.

--- GoldDigger/src/com/wasteofplastic/golddigger/digCommand.java
+++ GoldDigger/src/com/wasteofplastic/golddigger/digCommand.java
@@ -1,7 +1,6 @@
 package com.wasteofplastic.golddigger;
 
-import net.md_5.bungee.api.ChatColor;
-
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
